7ff2f9cc9a7e1a3fb66908a6eed869bd6b88e75d
r <r@a.ml>
2021-12-10 14:10:57 +0100

    added new loading animation

--- app/src/main/java/com/bijgepast/locationhunter/adapters/bindingAdapters.kt
+++ app/src/main/java/com/bijgepast/locationhunter/adapters/bindingAdapters.kt
@@ -1,9 +1,17 @@
 package com.bijgepast.locationhunter.adapters
 
+import android.content.Context
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
 import androidx.databinding.BindingAdapter
 import com.bijgepast.locationhunter.R
+import com.google.android.material.slider.Slider
 
 @BindingAdapter("isUnlocked")
 fun setSendState(v: ImageView, isUnlocked: Boolean) {
@@ -11,3 +19,18 @@ fun setSendState(v: ImageView, isUnlocked: Boolean) {
         if (isUnlocked) R.drawable.ic_baseline_lock_open_24 else R.drawable.ic_outline_lock_24
     v.setImageResource(drawableRes)
 }
+
+@BindingAdapter("goneUnless")
+fun goneUnless(view: View, visible: Boolean) {
+    view.visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("startAnimation")
+fun animation(v: ImageView, boolean: Boolean){
+    Log.d(null, "start Animation was called")
+    if(v.drawable is AnimationDrawable){
+        val a = v.drawable as AnimationDrawable
+        a.start()
+    }
+}
+
